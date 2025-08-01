183b9bb4a0a1d18224ae7c7cc77847d9805c11d2
TaytraX <gregoirengilot@gmail.com>
2025-08-01 19:40:31 -0400

    gdf

--- src/main/java/engine/maps/Level_1.java
+++ src/main/java/engine/maps/Level_1.java
@@ -1,4 +1,18 @@
 package engine.maps;
 
+import engine.AABB;
+import org.joml.Vector2f;
+
+import java.util.List;
+
 public class Level_1 {
+    public static List<AABB> platforms;
+
+    public void LoadMap(List<AABB> Map) {
+        platforms.add(new AABB(new Vector2f(0.0f, 0.0f), new Vector2f(1.0f, 1.0f)));
+        platforms.add(new AABB(new Vector2f(0.0f, 0.0f), new Vector2f(1.0f, 1.0f)));
+        platforms.add(new AABB(new Vector2f(0.0f, 0.0f), new Vector2f(1.0f, 1.0f)));
+        platforms.add(new AABB(new Vector2f(0.0f, 0.0f), new Vector2f(1.0f, 1.0f)));
+        platforms.add(new AABB(new Vector2f(0.0f, 0.0f), new Vector2f(1.0f, 1.0f)));;
+    }
 }
