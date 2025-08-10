f0a799012fb304a58cad506ce7c721d3d44583b9
TaytraX <gregoirengilot@gmail.com>
2025-08-10 14:57:01 -0400

    gdf

--- src/main/java/engine/ThreadManager.java
+++ src/main/java/engine/ThreadManager.java
@@ -12,15 +12,14 @@ public class ThreadManager {
     public static final AtomicReference<PlayerState> playerState  = new AtomicReference<>();
 
     public static void loadLevelAsync(int level) {
-        CompletableFuture<LevelManager> future = CompletableFuture.supplyAsync(() -> {
-            return switch(level) {
+        CompletableFuture<LevelManager> future = CompletableFuture.supplyAsync(() -> switch(level) {
                 case 2 -> new Level_2().load();
                 case 3 -> new Level_3().load();
                 case 4 -> new Level_4().load();
                 case 5 -> new Level_5().load();
                 default -> new Level_1().load();
-            };
-        });
+            }
+        );
 
         future.thenAccept(levelManager -> {
             platforms.put(level, levelManager);
